0fdaf2995062737231c1045b41794f646af81383
tastelikecoke <jmerc87@gmail.com>
2026-03-08 18:32:01 +0800

    Fix layout and logic

--- app/components/tictactoe.tsx
+++ app/components/tictactoe.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 
 function Cell({handleClick, content}: {handleClick: () => void, content: string}) {
-  return <div className="border-white border-[1px] size-[100px] text-[50px] text-center" onClick={handleClick}>
+  return <div className="border-white border-[1px] text-[50px] text-center size-[100px]" onClick={handleClick}>
     {content}
   </div>
 }
 function Row({handleClick, content}: {handleClick: (x: number) => void, content: string[]}) {
-  return <div className="columns-3">
+  return <div className="row">
     <Cell handleClick={() => handleClick(0)} content={content[0]} />
     <Cell handleClick={() => handleClick(1)} content={content[1]} />
     <Cell handleClick={() => handleClick(2)} content={content[2]} />
@@ -17,14 +17,31 @@ function Row({handleClick, content}: {handleClick: (x: number) => void, content:
 export default function TicTacToe() {
   const initialState = [["","",""],["","",""],["","",""]]
   const [gameState, setGameState] = useState(initialState)
+  const [currentTurn, setCurrentTurn] = useState(true)
+  const [winner, setWinner] = useState('')
 
   function handleClickCell(x: number, y: number) {
-    setGameState(gameState.map(
-      (row: string[], y_data) => {return row.map((cell, x_data) => (x === x_data && y === y_data) ? 'X' : cell)}))
+    const currentMarker = currentTurn ? 'X' : 'O' 
+    const nextState = gameState.map(
+      (row: string[], y_data) => {return row.map((cell, x_data) => (x === x_data && y === y_data) ? currentMarker : cell)})
+    setGameState(nextState)
+    setCurrentTurn(!currentTurn)
+    let isWin = false
+    isWin = isWin || nextState.some(row => row.every(cell => cell == 'X') || row.every(cell => cell == 'O'))
+    if (isWin)
+      setWinner(currentMarker)
   }
-  return <div className="">
+  return <div className="tictactoe">
+    {currentTurn ?
+      <p>X's Turn</p>
+      :
+      <p>O's Turn</p>
+    }
     <Row handleClick={(x: number) => handleClickCell(x, 0)} content={gameState[0]} />
     <Row handleClick={(x: number) => handleClickCell(x, 1)} content={gameState[1]} />
     <Row handleClick={(x: number) => handleClickCell(x, 2)} content={gameState[2]} />
+    {winner != '' &&
+      <p>{winner} Wins</p>
+    }
   </div>
 }
